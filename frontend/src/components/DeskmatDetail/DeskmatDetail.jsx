699b34bf4a0807bf946aaf8aeb5ce70ff98a1c7c
Vaibhav Sanadi <157675679+VaibhavSanadi007@users.noreply.github.com>
2025-07-25 19:14:56 +0530

    Update DeskmatDetail.jsx

--- frontend/src/components/DeskmatDetail/DeskmatDetail.jsx
+++ frontend/src/components/DeskmatDetail/DeskmatDetail.jsx
@@ -44,7 +44,7 @@ const DeskmatDetail = () => {
   };
 
   const getMatDetails = async (id) => {
-    const data = await axios.get(`http://localhost:3000/matSpecific/${id}`, {
+    const data = await axios.get(`https://ecom-2jj8.onrender.com/matSpecific/${id}`, {
       withCredentials: true,
     });
     setvalue(data.data);
