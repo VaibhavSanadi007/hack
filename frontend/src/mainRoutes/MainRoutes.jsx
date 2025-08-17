cd9359a342c71cc29908ae734a8853579c623d7c
vaibhav <vaibhavbhausanadi0007@gmail.com>
2025-08-17 20:51:04 +0530

    Added lazy loading with Suspense and route-level fallbacks

--- frontend/src/mainRoutes/MainRoutes.jsx
+++ frontend/src/mainRoutes/MainRoutes.jsx
@@ -1,28 +1,43 @@
-import {Route, Routes} from 'react-router-dom'
-import Home from '../components/Home'
-import About from '../components/about/About'
-import Mouse from '../components/Mouse/Mouse'
-import Keyboard from '../components/Keyboard/Keyboard'
-import Deskmat from '../components/Deskmat/Deskmat'
-import MouseDetail from '../components/MouseDetail/MouseDetail'
-import KeyDetail from '../components/Keydetail/KeyDetail'
-import DeskmatDetail from '../components/DeskmatDetail/DeskmatDetail'
-import Login from '../components/auth/Login'
-import Cart from '../components/cart/Cart'
+import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Home from "../components/Home";
+import About from "../components/about/About";
+const MouseLazy = lazy(() => import("../components/Mouse/Mouse"));
+const KeyLazy = lazy(() => import("../components/Keyboard/Keyboard"));
+const MatLazy = lazy(() => import("../components/Deskmat/Deskmat"));
+const MouseDetailLazy = lazy(() =>
+  import("../components/MouseDetail/MouseDetail")
+);
+const KeyDetailLazy = lazy(() => import("../components/Keydetail/KeyDetail"));
+const MateDetailLazy = lazy(() =>
+  import("../components/DeskmatDetail/DeskmatDetail")
+);
+
+import Login from "../components/auth/Login";
+import Cart from "../components/cart/Cart";
 const MainRoutes = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/mouse' element={<Mouse/>} />
-      <Route path='/mousedetail/:id' element={<MouseDetail/>} />
-      <Route path='/keyboard' element={<Keyboard/>} />
-      <Route path='/keydetail/:id' element={<KeyDetail/>} />
-      <Route path='/deskmat' element={<Deskmat/>} />
-      <Route path='/matdetail/:id' element={<DeskmatDetail/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/cart' element={<Cart/>} />
-    </Routes>
-  )
-}
-export default MainRoutes
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/mouse"
+          element={
+            <Suspense fallback={<div>Mouse lazy loading...</div>}>
+              <MouseLazy />
+            </Suspense>
+          }
+        />
+        <Route path="/mousedetail/:id" element={<MouseDetailLazy />} />
+        <Route path="/keyboard" element={<KeyLazy />} />
+        <Route path="/keydetail/:id" element={<KeyDetailLazy />} />
+        <Route path="/deskmat" element={<MatLazy />} />
+        <Route path="/matdetail/:id" element={<MateDetailLazy />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Suspense>
+  );
+};
+export default MainRoutes;
